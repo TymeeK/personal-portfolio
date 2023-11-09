40213ae66246325914009911277bf2e4b9dc22d2
Tymee Kong <tymeekong562@gmail.com>
2023-11-08 16:42:58 -0800

    Added typing animation for home page

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -4,6 +4,7 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Image from 'react-bootstrap/Image';
 import NavBar from '../Navigation/NavBar';
+import ReactTyped from 'react-typed';
 export default function Home() {
     return (
         <>
@@ -11,13 +12,30 @@ export default function Home() {
             <div id='about'>
                 <Container>
                     <Row className='about-container'>
-                        <Col className='description-container mt-5'>
-                            <h2 className='lead'>
-                                I am a CSULB Computer Science graduate. I
-                                specialize in web development and I am
-                                proficient in front-end technologies such as
+                        <Col className='description-container'>
+                            <div>
+                                <h1>Hi there 👋</h1>
+                            </div>
+                            <div>
+                                <h1 className='typewriter'>
+                                    <ReactTyped
+                                        strings={[
+                                            'Welcome to my personal website!',
+                                        ]}
+                                        typeSpeed={50}
+                                        loop
+                                        backSpeed={50}
+                                    />
+                                </h1>
+                            </div>
+                            {/* <h2>I am a CSULB Computer Science graduate.</h2>
+                            <br />
+                            <h2>I specialize in web development</h2>
+                            <br />
+                            <h2>
+                                Proficient in front-end technologies such as
                                 HTML, CSS, JavaScript, and ReactJS.
-                            </h2>
+                            </h2> */}
                         </Col>
                         <Col className='mt-5 mb-5 image-container'>
                             <Image
